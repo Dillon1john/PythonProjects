8a6a4bb4445deb2bc6f5119dfba659ffb906da9a
Dillon1john <50587936+Dillon1john@users.noreply.github.com>
2021-09-16 01:56:31 -0400

    Add files via upload

---
+++ flight-deals-start/main.py
@@ -0,0 +1,51 @@
+from data_manager import DataManager
+from pprint import pprint
+from datetime import *
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+ORIGIN_CITY_IATA = "NYC"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA, destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today)
+
+    if flight is None:
+        continue
+
+    if flight.price < destination["lowestPrice"]:
+        users = data_manager.get_customer_emails()
+        emails = [row["email"] for row in users]
+        names = [row["firstName"] for row in users]
+
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to " \
+                  f"{flight.destination_city}-{flight.destination_airport}," \
+                  f" from {flight.out_date} to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}"
+            print(message)
+        link = f"https://www.google.co.uk/flights?hl=en#flt={flight.origin_airport}.{flight.destination_airport}." \
+               f"{flight.out_date}*{flight.destination_airport}.{flight.origin_airport}.{flight.return_date}"
+        notification_manager.send_emails(emails, message, link)
+        # notification_manager.send_message(message)
+
+
+
